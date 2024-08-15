6c6bd7ca557900e493a4817afdb46d5bce670db0
acenmm23 <133246880+acenmm23@users.noreply.github.com>
2024-08-15 11:15:29 -0700

    plswork

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+function openProject(evt, projectName) {
+    var i, tabcontent, tablinks;
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+    // Hide all tabcontent elements
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove active class from all buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    // Show the current tab and add active class to the button
+    document.getElementById(projectName).style.display = "block";
+    evt.currentTarget.style.backgroundColor = "#333";
+}
+
+// Default open tab
+document.getElementsByClassName("tablink")[0].click();
